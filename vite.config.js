4671791add0c8e83f657d7e6eaec53db94de05b6
Donovan Gall <dgall6@my.wctc.edu>
2026-01-30 23:09:42 -0600

    [1.08] Changed base of vite.config.js to reflect new repo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/module-1-assignment-medals-react/',
+  base: 'https://dgall6.github.io/medal-react-module-3/',
   plugins: [react()],
 })
