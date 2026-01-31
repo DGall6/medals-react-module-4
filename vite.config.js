158ea641b07837cb6918601aab18c3101afc80c9
Donovan Gall <dgall6@my.wctc.edu>
2026-01-30 23:20:29 -0600

    [1.10] Reverting to previous changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://dgall6.github.io/medal-react-module-3/',
+  base: '/module-1-assignment-medals-react/',
   plugins: [react()],
 })
