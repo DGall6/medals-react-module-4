4674cd9bf3bf342705375db434b8bae86bde67fe
Donovan Gall <dgall6@my.wctc.edu>
2026-02-09 00:11:45 -0600

    [1.16] Updated config file to point to right repo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/medal-react-module-3/',
+  base: '/medals-react-module-4/',
   plugins: [react()],
 })
